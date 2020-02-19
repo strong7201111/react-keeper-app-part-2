4859bd13f78f02de676161ceb95b5b84bb6e44c6
strong7201111 <37434867+strong7201111@users.noreply.github.com>
2020-02-19 07:14:48 +0300

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function createNote(note) {
+  return <Note key={note.key} title={note.title} content={note.content} />;
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map(createNote)}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
